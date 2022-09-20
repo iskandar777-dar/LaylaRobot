00171cad0ad6bcf2a2100af876ca404d679c54ef
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-09-20 12:03:14 +0700

    Update truth_and_dare.py

--- LaylaRobot/modules/truth_and_dare.py
+++ LaylaRobot/modules/truth_and_dare.py
@@ -6,6 +6,7 @@ from telegram import ParseMode, Update, Bot
 from LaylaRobot.modules.disable import DisableAbleCommandHandler
 from telegram.ext import CallbackContext, run_async
 
+
 @run_async
 def truth(update: Update, context: CallbackContext):
     args = context.args
@@ -16,11 +17,17 @@ def dare(update: Update, context: CallbackContext):
     args = context.args
     update.effective_message.reply_text(random.choice(truth_and_dare_string.DARE))
 
+@run_async
+acak = ["Kamu dapat truth, klik /truth", "Kamu dapat dare, klik /dare"]
+def truth(update: Update, context: CallbackContext):
+    args = context.args
+    update.effective_message.reply_text(random.choice(acak))
     
+
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
-
+ACAK_HANDLER = DisableAbleCommandHandler("truth", acak)
 
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
-
+dispatcher.add_handler(ACAK_HANDLER)
